1b375f4f67f8540045d4dd39c4157909c90b5e03
Kenneth Bondesen <4944294+bondesen@users.noreply.github.com>
2026-07-10 14:29:31 +0200

    Fjern ubrugt run.sh (binaeren koeres direkte, ingen bashio)

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-export GAMER_MAC="$(bashio::config 'gamer_mac')"
-export GAMER_URL="$(bashio::config 'gamer_url')"
-export GAMER_TCP="$(bashio::config 'gamer_tcp')"
-export BROADCAST="$(bashio::config 'broadcast')"
-export LISTEN=":$(bashio::config 'listen_port')"
-bashio::log.info "WoL Ollama Proxy starter (mac ${GAMER_MAC}, mål ${GAMER_URL}, port ${LISTEN})"
-exec /usr/bin/wolproxy
